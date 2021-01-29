ba8fd4b32faa2f553452ce7a3e156c58006e6aff
orieuxe <orieux.etienne@gmail.com>
2021-01-29 22:59:41 +0100

    test(package): fix package name

--- app/src/androidTest/java/android/eservices/pogchamps/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/android/eservices/pogchamps/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("android.eservices.webrequests", appContext.getPackageName());
+        assertEquals("android.eservices.pogchamps", appContext.getPackageName());
     }
 }
